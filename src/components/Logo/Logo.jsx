aac19b12ea5f2bd0509925f68d8a410734cb4b4b
Dmitry Kologrivko <dmitrykologrivko@gmail.com>
2024-09-30 12:57:44 +0300

    Updated logo and favicon

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,9 +1,9 @@
-import regionFlag from '../../assets/region-flag.svg';
+import code from '../../assets/code.svg';
 
 function Logo() {
   return (
     <div className="flex items-center text-white font-bold">
-      <img src={regionFlag} className="h-10 pr-2" alt="Logo"/>
+      <img src={code} className="h-10 pr-2" alt="Logo"/>
       IT26
     </div>
   );
